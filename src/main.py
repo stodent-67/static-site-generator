4ebb1fbd0e321d0a92cf104b8d83d829c2306ef7
stodent-67 <thomas.gavin.25@gmail.com>
2026-01-17 20:03:05 +1100

    main and textnode object created

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import*
+
+def main():
+    dummy = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(dummy)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,17 @@ class TextType(Enum):
 	CODE_TEXT = "code"
 	LINK_TEXT = "link"
 	IMAGE_TEXT = "image"
+
+class TextNode:
+	def __init__(self, text, text_type, url=None):
+		self.text = text
+		self.text_type = TextType(text_type)
+		self.url = url
+
+	def __eq__(self, other):
+		if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+			return True
+		return False
+	
+	def __repr__(self):
+		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
